b46b891abbd4ef9d687d6258672273d0b22ccffa
Nick Dell'Aquilo <nickdellaquilo@proton.me>
2025-06-11 00:08:27 -0400

    User input

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import os
+import sys
 from dotenv import load_dotenv
+from google import genai
+
+if len(sys.argv) < 2:
+    print("ERROR: No input message")
+    exit(1)
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-from google import genai
-
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model = "gemini-2.0-flash-001",
-    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    #contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents = sys.argv[1]
     )
 
 print(response.text)
