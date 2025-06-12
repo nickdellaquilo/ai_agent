2c54e4e6f49851835aca6021f7665ebbd482dc1a
Nick Dell'Aquilo <nickdellaquilo@proton.me>
2025-06-12 16:39:25 -0400

    Prompt storage, verbose argument

--- main.py
+++ main.py
@@ -2,21 +2,36 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 if len(sys.argv) < 2:
     print("ERROR: No input message")
     exit(1)
 
+user_prompt = sys.argv[1]
+verbose = False
+
+if len(sys.argv) > 2:
+    verbose = ("--verbose" in sys.argv)
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
 
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model = "gemini-2.0-flash-001",
-    #contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    contents = sys.argv[1]
+    contents = messages
     )
 
+
+
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
