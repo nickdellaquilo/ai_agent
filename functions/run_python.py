20267a7bc5193063aa69d15f08e74409b5c008c1
Nick Dell'Aquilo <nickdellaquilo@proton.me>
2025-07-23 20:54:51 -0400

    all function call schemas

--- functions/run_python.py
+++ functions/run_python.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from google.genai import types
 
 def run_python_file(working_directory, file_path, args=[]):
     abs_working_dir = os.path.abspath(working_directory)
@@ -28,3 +29,17 @@ def run_python_file(working_directory, file_path, args=[]):
         return result
     except Exception as e:
         return f"Error: executing Python file: {e}"
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Executes a specified Python file with optional arguments.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file+path": types.Schema(
+                type=types.Type.STRING,
+                description="The file path to the Python file to be run, relative to the working directory.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.run_python import schema_run_python_file
+from functions.write_file import schema_write_file
 
 if len(sys.argv) < 2:
     print("ERROR: No input message")
@@ -16,6 +19,9 @@ You are a helpful AI coding agent.
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
 - List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
@@ -25,6 +31,9 @@ verbose = False
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file
     ]
 )
 
